61a3e996e6c86c4f00b48387c6dc002ea7c7f450
Kaitlin Rosen <kayejrosen@gmail.com>
2022-04-04 16:49:38 -0700

    completed countWords function

--- objects.js
+++ objects.js
@@ -3,9 +3,24 @@
 // 1. countWords
 function countWords(phrase) {
   // Replace this with your code
+  const wordCount = {};
+  
+  for (const word of phrase.split(' ')) {
+    if (wordCount[word]) {
+      wordCount[word] += 1
+    } else {
+      wordCount[word] = 1
+    }
+  }
+
+  return wordCount
 }
+
+
 
 // 2. getMelonsAtPrice
 function getMelonsAtPrice(price) {
   // Replace this with your code
+
+  
 }
