603ccb77695f8c1f888d12192fd6cff3cb46a94d
Kaitlin Rosen <kayejrosen@gmail.com>
2022-04-04 15:54:14 -0700

    First Two Arrays Complete

--- arrays.js
+++ arrays.js
@@ -3,12 +3,30 @@
 // 1. printIndices
 function printIndices(items) {
   // Replace this with your code
+
+  //create a for loop 
+  for (const index in items) {
+    console.log(`${items[index]} ${index}`);
+  }
 }
+
 
 // 2. everyOtherItem
 function everyOtherItem(items) {
   // Replace this with your code
+
+  const result = []
+
+  for (const i in items) {
+    if (i % 2 === 0) {
+      result.push(items[i]);
+    }
+  }
+
+  console.log(result);
+
 }
+
 
 // 3. smallestNItems
 function smallestNItems(items, n) {
