2a3c6d9294f580768f5c7950f85c74b76a19dab4
Kaitlin Rosen <kayejrosen@gmail.com>
2022-04-04 17:07:46 -0700

    Finished objects.js

--- objects.js
+++ objects.js
@@ -22,5 +22,18 @@ function countWords(phrase) {
 function getMelonsAtPrice(price) {
   // Replace this with your code
 
-  
+  const melonPrices = {
+    '2.50': ['Cantaloupe', 'Honeydew'],
+    '2.95': ['Watermelon'],
+    '3.25': ['Musk', 'Crenshaw'],
+    '14.25': ['Christmas']
+  };
+
+
+  if (melonPrices.hasOwnProperty(price)) {
+    return melonPrices[price].sort();
+  } else {
+    return null;
+  }
+
 }
